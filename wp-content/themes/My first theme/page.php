1c82089cde6a9054b8acaa7fa2b0105d063d89ad
Grace Aloo <graceaaloo@gmail.com>
2025-06-12 21:49:55 +0300

    learning about parent and children pages

--- wp-content/themes/My first theme/page.php
+++ wp-content/themes/My first theme/page.php
@@ -17,19 +17,43 @@
 
   <div class="container container--narrow page-section">
 
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+    <?php 
+      $parent = wp_get_post_parent_id(get_the_ID());
+       if ($parent) {?>
+       <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
     </div>
+        
+       <?php }
+     ?>
     
-    <!--
+    
+   <?php
+   $testArray = get_pages(array(
+    'child_of' => get_the_ID()
+   ));
+   if ($parent or $testArray ) { ?>
     <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($parent) ?>"><?php echo get_the_title($parent) ?></a></h2>
       <ul class="min-list">
+        <?php
+        if ($parent) {
+           $findchildof = $parent;
+        } else {
+            $findchildof = get_the_ID();
+        }
+
+        wp_list_pages(array(
+          'title_li' => null,
+          'child_of' => $findchildof,
+          'sort_column' => 'menu_order'
+        ));
+         ?>
         <li class="current_page_item"><a href="#">Our History</a></li>
         <li><a href="#">Our Goals</a></li>
       </ul>
     </div>
-    -->
+    <?php } ?>
 
     <div class="generic-content">
       <?php the_content(); ?>
